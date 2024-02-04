7b4441a769befc50f94508576f9c03f0fb74ade4
Kate Filipovich <xkoshka@mail.ru>
2024-02-04 18:23:09 +0300

    add Lesson2 ;task1

---
+++ 01 CSharp/Lesson2/Task1/Program.cs
@@ -0,0 +1,16 @@
+﻿int [] arr = new int [5];
+arr[0] = 5;
+arr[1] = 2;
+arr[2] = 53;
+arr[3] = 7;
+arr[4] = 9;
+
+Console.WriteLine(arr[0]);
+Console.WriteLine(arr[1]);
+Console.WriteLine(arr[2]);
+Console.WriteLine(arr[3]);
+Console.WriteLine(arr[4]);
+
+int [] arr2 = new int [5] { 3, 5, 6, 8, 10};
+
+int [] arr3 = { 3, 5, 6, 8, 10};
